1e4585ca2fdf594279c464b08f705f5f686a3482
toris-div <ironjustlikethat@gmail.com>
2024-04-02 16:50:51 +0900

    🚩 Feat: questionnaireRouter add

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { userRouter } from './user/index.controller';
+import { questionnaireRouter } from './questionnaire/index.controller';
 
 export const router = express.Router();
 
 router.use('/oauth', userRouter);
+router.use('/question', questionnaireRouter);
--- src/controller/questionnaire/index.controller.js
+++ src/controller/questionnaire/index.controller.js
@@ -1,27 +1,24 @@
 import { Router } from 'express';
 import { db } from '../../utils/firebase';
 
-export const router = Router();
+export const questionnaireRouter = Router();
 
 // 질문지 작성
 // 요청값: message -> string, 토큰 존재여부 O
-router.post('/', (req, res) => {
+questionnaireRouter.post('/', (req, res) => {
   res.send(req.body);
 });
 
 // 질문지에 대한 메시지 전체 받아오기
-router.get('/:questionnaireId', (req, res) => {
+questionnaireRouter.get('/:questionnaireId', (req, res) => {
   const { questionnaireId } = req.params;
-  const messages = db
-    .collection('elice')
-    .doc('questionList')
-    .get(questionnaireId);
+  const messages = db.collection('elice').doc(questionnaireId).get();
 
   res.json({ questionnaireId: messages });
 });
 
 // 메시지 1개 받아오기
-router.get('/:questionnaireId/:questionId', async (req, res) => {
+questionnaireRouter.get('/:questionnaireId/:questionId', async (req, res) => {
   const { questionnaireId, questionId } = req.params;
 
   const message = db.collection('elice').doc(questionnaireId).get(questionId);
@@ -32,7 +29,7 @@ router.get('/:questionnaireId/:questionId', async (req, res) => {
 });
 
 // 질문 메시지 작성
-router.get('/:questionnaireId/question', (req, res) => {
+questionnaireRouter.get('/:questionnaireId/question', (req, res) => {
   const { questionnaireId } = req.params;
   const { message, icon } = req.body;
 
@@ -45,7 +42,7 @@ router.get('/:questionnaireId/question', (req, res) => {
   res.send(req.body);
 });
 
-router.delete('/:questionId', (req, res) => {
+questionnaireRouter.delete('/:questionId', (req, res) => {
   const { questionId } = req.params;
 
   res.send(questionId);
