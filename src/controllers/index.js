69987379a887115136cf7cb5295737b763812520
toris <ironjustlikethat@gmail.com>
2024-04-03 18:04:54 +0900

    Refactor: 코드 구조 변경

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { questionRouter } from './question.controller';
-import { questionnaireRouter } from './questionnaire.controller';
-import { userRouter } from './user.controller';
+import { messageRouter } from './message.controller';
+import { oauthRouter } from './oauth.controller';
+import { treeRouter } from './tree.controller';
 
 export const router = express.Router();
 
-router.use('/oauth', userRouter); // 소셜로그인에 대한 Router
-router.use('/questionnaire', questionnaireRouter); // 메시지에 대한 Router
-router.use('/question', questionRouter); // 질문지에 대한 Router
+router.use('/oauth', oauthRouter); // 소셜로그인에 대한 Router
+router.use('/message', messageRouter); // 트리에 대한 Router
+router.use('/tree', treeRouter);
--- src/service/kakaoAuth.service.js
+++ src/service/kakaoAuth.service.js
@@ -4,8 +4,8 @@ import { auth } from '../utils/firebase';
 export const getToken = async (code) => {
   const body = {
     grant_type: 'authorization_code',
-    client_id: 'c84840525d9cdbf1a8ba40a6b6f62089',
-    redirect_uri: 'http://localhost:5173/kakaotalk',
+    client_id: process.env.KAKAO_CLIENT_ID,
+    redirect_uri: process.env.KAKAO_REDIRECT_URI,
     code
   };
 
@@ -13,20 +13,23 @@ export const getToken = async (code) => {
     'https://kauth.kakao.com/oauth/token',
     new URLSearchParams(body)
   );
-  console.log(res);
   return res.data;
 };
+
+// id_token 은 저장해도 될 것 같다.
+// DB저장 -> access_token(선택), refresh_token(필수)
 export const getKakaoUser = async (token) => {
   const res = await axios.get('https://kapi.kakao.com/v2/user/me', {
     headers: { Authorization: `Bearer ${token}` }
   });
+  console.log(res);
   return res.data;
 };
 export const updateOrCreateUser = async (user) => {
   const kakaoAccount = user.kakao_account;
   const properties = {
     uid: `kakao:${user.id}`,
-    provider: 'KAKAO',
+    provider: 'kakao',
     displayName: kakaoAccount?.profile?.nickname,
     email: kakaoAccount?.email
   };
@@ -35,9 +38,33 @@ export const updateOrCreateUser = async (user) => {
     return await auth.updateUser(properties.uid, properties);
   } catch (error) {
     if (error.code === 'auth/user-not-found') {
-      // eslint-disable-next-line no-return-await
-      return await auth.createUser(properties);
+      return auth.createUser(properties);
     }
     throw error;
   }
 };
+
+export async function refreshAccessToken(refreshToken) {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+      }
+    };
+
+    const params = new URLSearchParams();
+    params.append('grant_type', 'refresh_token');
+    params.append('client_id', process.env.KAKAO_CLIENT_ID);
+    params.append('refresh_token', refreshToken);
+
+    const response = await axios.post(
+      'https://kauth.kakao.com/oauth/token',
+      params,
+      config
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error refreshing access token:', error);
+    throw error;
+  }
+}
