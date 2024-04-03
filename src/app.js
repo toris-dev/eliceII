5c0becf28313c6c04fe36f04e51f6a08b97b61e8
toris <ironjustlikethat@gmail.com>
2024-04-03 18:02:20 +0900

    ➕ Chore: cookie-parser 추가

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { configDotenv } from 'dotenv';
 import express from 'express';
@@ -8,6 +9,7 @@ configDotenv();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', router);
 
