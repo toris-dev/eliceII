f6d0a7acb6febe46b3bde3987c16d5682a011b8c
toris <ironjustlikethat@gmail.com>
2024-04-07 15:44:32 +0900

    ♻️ Refactor: question controller service 분리

--- src/controllers/question.controller.js
+++ src/controllers/question.controller.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import verifyAuthToken from '../middleware/oauth.middleware';
-import { db } from '../utils/firebase';
+import QuestionService from '../service/question.service';
 
 export const questionRouter = Router();
+const question = new QuestionService();
 
 // 질문 작성, 미들웨어 추가 예정
 questionRouter.post('/', verifyAuthToken, async (req, res) => {
@@ -10,21 +11,14 @@ questionRouter.post('/', verifyAuthToken, async (req, res) => {
     const message = req.body;
     const { uid } = req.user;
     // tree 컬렉션에서 uid 필드 값이 미들웨어에서 검사한 uid 와 일치하는 문서 조회
-    const querySnapshot = await db
-      .collection('tree')
-      .where('uid', '==', uid)
-      .get();
-    const docRef = querySnapshot.docs[0].ref;
-    await docRef.update({
-      question: message
-    });
+    const { treeId } = await question.createQuestion(uid, message);
     res.status(200).json({
-      message: '질문이 등록되었습니다.'
+      message: '질문이 등록되었습니다.',
+      treeId
     });
   } catch (error) {
-    res.status(404).json({
-      message: '질문이 등록되지 않았습니다.',
-      error
+    res.status(500).json({
+      message: '질문이 등록되지 않았습니다.'
     });
     throw new Error(error);
   }
