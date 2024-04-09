6e6c73cc9246a07dea9576cb03ce12fd96944a71
toris <ironjustlikethat@gmail.com>
2024-04-09 14:18:04 +0900

    ✏️ Fix: 응답을 redirect 가 아닌 json 으로 전환

--- src/controllers/oauth.controller.js
+++ src/controllers/oauth.controller.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import jwt from 'jsonwebtoken';
 import { jwtSecretKey } from '../constant/env';
-import { frontendUrl } from '../constant/url';
 import OAuthService from '../service/oauth.service';
 
 export const oauthRouter = Router();
@@ -21,7 +20,6 @@ oauthRouter.get('/kakao', async (req, res) => {
     const response = await authKakao.getToken(code);
     const token = response.access_token;
     const kakaoUser = await authKakao.getUser(token);
-    const userCheck = await authKakao.userCheck(kakaoUser.id);
 
     let authUser;
     try {
@@ -41,11 +39,10 @@ oauthRouter.get('/kakao', async (req, res) => {
     const treeId = await authKakao.userTreeFind(authUser.uid);
 
     // 질문 입력 여부에 따라 리디렉션할 경로 결정
-    const redirectPath = userCheck ? `/host/tree/${treeId}` : '/host/question';
     return res
       .cookie('accessToken', accessToken, { httpOnly: true })
       .cookie('kakaoToken', token, { httpOnly: true })
-      .redirect(frontendUrl + redirectPath);
+      .json({ treeId });
   } catch (error) {
     console.error('Error occurred:', error);
     return res.status(500).json({ message: '카카오 로그인에 실패했습니다.' });
@@ -64,7 +61,6 @@ oauthRouter.get('/naver', async (req, res) => {
 
     const response = await authNaver.getToken(code); // 네이버 OAuth를 통해 액세스 토큰을 받아옴
     const naverUser = await authNaver.getUser(response.access_token); // 액세스 토큰을 사용하여 네이버 사용자 정보를 가져옴
-    const userCheck = await authNaver.userCheck(naverUser.id);
     // 이후에 필요한 처리를 수행하고 클라이언트에게 응답을 보냄
     let authUser;
     try {
@@ -82,11 +78,10 @@ oauthRouter.get('/naver', async (req, res) => {
     const treeId = await authNaver.userTreeFind(authUser.uid);
 
     // 질문 입력 여부에 따라 리디렉션할 경로 결정
-    const redirectPath = userCheck ? `/host/tree/${treeId}` : '/host/question';
     return res
       .cookie('accessToken', accessToken, { httpOnly: true })
       .cookie('naverToken', response.access_token, { httpOnly: true })
-      .redirect(frontendUrl + redirectPath);
+      .json({ treeId });
   } catch (error) {
     res.status(500).json({ message: '네이버 로그인에 실패하였습니다.' });
     throw new Error(error);
